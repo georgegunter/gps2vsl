eebe937ffe2b90881a21f9643847cacbb91e09eb
George Gunter <gunter.gl@gmail.com>
2025-01-27 10:56:07 -0600

    maybe fixes where the launch file is found for cbf

--- cbf/installCbfController.sh
+++ cbf/installCbfController.sh
@@ -19,7 +19,10 @@ source ~/.bashrc
 pushd ~/catkin_ws
 source devel/setup.sh
 #rosrun robot_upstart uninstall can
-rosrun robot_upstart install can_to_ros/launch/${LAUNCH_FILE} --user root
+#rosrun robot_upstart install can_to_ros/launch/${LAUNCH_FILE} --user root
+rosrun robot_upstart install ${LAUNCH_FILE} --user root
+
+
 
 echo "Enabling can_to_ros startup script"
 sudo systemctl daemon-reload
